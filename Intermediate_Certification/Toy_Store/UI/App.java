db9159eb9d85d04c301b4bfd84998b88a3613e6b
Adil <christie_m@mail.ru>
2023-03-05 15:54:17 +0300

    Intermediate_Certification2

--- Intermediate_Certification/Toy_Store/UI/App.java
+++ Intermediate_Certification/Toy_Store/UI/App.java
@@ -16,28 +16,28 @@ public class App {
 
         while (true) {
 
-            command = prompt("
-                     "1" - "Добавьте игрушку к рисунку"
-                     "2" - "Удалить игрушку из рисунка"
-                     "3" - "Сделайте рисунок (покажите результаты)"
-                     "4" - "Покажите игрушки для рисования"
-                     "5" - "Очистить все записи"
-                     "6" - "Сохраните все записи в файл"
-                     "7" - "Загрузите все записи из файла"
-                     "8" - "Выход"
-                    ");
+            command = prompt("""
+                     1 - Добавьте игрушку к рисунку
+                     2 - Удалить игрушку из рисунка
+                     3 - Сделайте рисунок (покажите результаты)
+                     4 - Покажите игрушки для рисования
+                     5 - Очистить все записи
+                     6 - Сохраните все записи в файл
+                     7 - Загрузите все записи из файла
+                     8 - Выход
+                    Сделайте свой выбор:\s""");
             if (command.equals("8")) {
                 return;
             }
             try {
                 switch (command) {
-                    case 1 -> presenter.putForDrawing();
-                    case 2 -> presenter.deleteFromDrawing();
-                    case 3 -> presenter.getDrawing();
-                    case 4 -> presenter.showAll();
-                    case 5 -> presenter.clearAll();
-                    case 6 -> presenter.saveToFile();
-                    case 7 -> presenter.loadFromFile();
+                    case "1" -> presenter.putForDrawing();
+                    case "2" -> presenter.deleteFromDrawing();
+                    case "3" -> presenter.getDrawing();
+                    case "4" -> presenter.showAll();
+                    case "5" -> presenter.clearAll();
+                    case "6" -> presenter.saveToFile();
+                    case "7" -> presenter.loadFromFile();
                     default -> System.out.println("\n Команда не найдена!");
                 }
             } catch (Exception e) {
